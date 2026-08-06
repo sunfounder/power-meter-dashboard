fe3c754844fad94a359d663c27df3b166c291191
Cavon <cavonxx@gmail.com>
2026-08-06 15:58:04 +0800

    NTP: 3-server config (2 domain + 1 IP)

--- firmware/src/net/server.cpp
+++ firmware/src/net/server.cpp
@@ -123,17 +123,13 @@ void PowerMeterWebServer::update() {
   }
 }
 
-// NTP sync with China-friendly servers (domain + direct IPs to dodge DNS issues)
+// NTP sync with China-friendly servers (2 domains + 1 direct IP to dodge DNS issues)
 void ntpSync() {
   int8_t tz = DeviceSettings::getInstance().tzOffset();
   configTime(tz * 3600, 0,
              "ntp.aliyun.com",
-             "203.107.6.88",        // ntp.aliyun.com direct IP
              "ntp.tencent.com",
-             "120.25.115.20",       // ntp.tencent.com direct IP
-             "cn.pool.ntp.org",
-             "time.nist.gov",
-             "time.google.com");
+             "203.107.6.88");   // ntp.aliyun.com direct IP
 }
 
 void PowerMeterWebServer::startAP() {
